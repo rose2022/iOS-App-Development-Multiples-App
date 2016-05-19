4bd7ba7f2376bd5d9489644f365c7914e378c027
Rosemary Espinal <rosemaryespinal@Rosemarys-MacBook-Pro.local>
2016-05-18 20:13:36 -0400

    Multiples app code and app screen shots

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  /*
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,80 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  */
+    
+    //Outlets for UI components
+    
+    @IBOutlet weak var multiplesLogo: UIImageView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var addButtonLogo: UILabel!
+    
+    @IBOutlet weak var resultsFieldLabel: UILabel!
+    
+    //Global variables
+    let startingValue = 0;
+    var textFieldInputValue = 0;
+    var runningSum = 0;
+    var currentSum = 0;
+    
+    //On play button click
+    @IBAction func onPlayButtonClick (sender: UIButton) {
+        
+        textFieldInputValue = Int(textField.text!)!
+        
+        //check to see if the text field is empty, if so dont start the game
+        if textFieldInputValue == 0 {
+            return;
+        } else {
+            startGame();
+        }
 
-
+    }
+    
+    @IBAction func onAddButtonClick (sender: UIButton) {
+        //hide the addButtonLogo
+        addButtonLogo.hidden = true;
+        
+        //display results
+        displayResults()
+        
+    }
+    
+    //function to keep track of current running sum based on user input number
+    func add() -> Int {
+        currentSum += textFieldInputValue
+        return currentSum
+    }
+    
+    func displayResults() {
+        //calculate the overall running sum
+        runningSum = add() + textFieldInputValue
+        
+        //display results
+        resultsFieldLabel.text = "\(textFieldInputValue)" + " + " + "\(currentSum)" + " = " + "\(runningSum)"
+    }
+    
+    func startGame(){
+        //unhide the results text field
+        resultsFieldLabel.hidden = false;
+        resultsFieldLabel.text = "\(startingValue)";
+        
+        //hide play button, multiples logo, text field
+        playButton.hidden = true;
+        multiplesLogo.hidden = true;
+        textField.hidden = true;
+        
+        
+        //show the add button and add label
+        addButton.hidden = false;
+        addButtonLogo.hidden = false;
+    }
+    
 }
 
